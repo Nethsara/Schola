73121930f404dcd6deb816352bc83a4d9533f333
NethsaraSiyum <nethsara@apium.io>
2022-11-29 03:43:25 +0530

    Light mode dashboards

--- src/me/siyum/schola/controller/lecturers/LecturersPageController.java
+++ src/me/siyum/schola/controller/lecturers/LecturersPageController.java
@@ -1,7 +1,11 @@
 package me.siyum.schola.controller.lecturers;
 
+import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 import me.siyum.schola.util.Navigation;
 import me.siyum.schola.util.Routes;
 
@@ -9,6 +13,22 @@ import java.io.IOException;
 
 public class LecturersPageController {
     public AnchorPane mainPane;
+    public Circle circleImg;
+    public JFXButton btnDash;
+    public FontAwesomeIconView iconDash;
+    public JFXButton btnStudent;
+    public FontAwesomeIconView iconStudents;
+    public JFXButton btnExam;
+    public FontAwesomeIconView iconExam;
+    public JFXButton btnClass;
+    public FontAwesomeIconView iconClass;
+    public JFXButton btnAttendance;
+    public FontAwesomeIconView iconAttendance;
+    public JFXButton btnHomeWork;
+    public FontAwesomeIconView iconHomeWork;
+    public JFXButton btnSalary;
+    public FontAwesomeIconView iconSalary;
+    public Circle circleImgLeft;
 
     public void initialize() throws IOException {
         Navigation.navigate(Routes.LECTURER_DASHBOARD, mainPane);
@@ -38,5 +58,11 @@ public class LecturersPageController {
 
     public void attendancePage(ActionEvent actionEvent) throws IOException {
         Navigation.navigate(Routes.LECTURER_ATTENDANCE, mainPane);
+    }
+
+    public void homeWorks(ActionEvent actionEvent) {
+    }
+
+    public void logout(MouseEvent mouseEvent) {
     }
 }
