97578f2c6ac990758e207018f384eb499f6b3587
NethsaraSiyum <nethsara@apium.io>
2022-11-29 18:32:26 +0530

    Light mode dashboards

--- src/me/siyum/schola/AppInitializer.java
+++ src/me/siyum/schola/AppInitializer.java
@@ -18,7 +18,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        Parent root = FXMLLoader.load((getClass().getResource("view/headers/LecturerPage.fxml")));
+        Parent root = FXMLLoader.load((getClass().getResource("view/students/StudentForm.fxml")));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         primaryStage.centerOnScreen();
--- src/me/siyum/schola/util/Env.java
+++ src/me/siyum/schola/util/Env.java
@@ -3,5 +3,5 @@ package me.siyum.schola.util;
 public class Env {
     public static String file = "C:/Schola";
     public static String downloadLocation = "C:/Schola/Downloads/";
-    public static String token = "xxxxx";
+    public static String token = "qqqqq";
 }
